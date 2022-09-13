6f99a65b80437ff24609a8df42f625d1677afff4
akulawiec <akulawiec@meil.pw.edu.pl>
2022-09-13 22:02:24 +0200

    Added Owner Index Page and Controller. Closes #13

--- pet-clinic-web/src/main/java/adi/art/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/adi/art/sfgpetclinic/controllers/OwnerController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
-
+@RequestMapping("/owners")
+public class OwnerController {
     @GetMapping({"/",""})
     public String listVets(){
-        return "vets/index";
+        return "owners/index";
     }
 }
