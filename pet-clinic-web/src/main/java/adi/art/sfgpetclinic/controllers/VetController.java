45388e51b2793d7a8d809452a3037748b7ba1700
akulawiec <akulawiec@meil.pw.edu.pl>
2022-09-13 21:56:27 +0200

    Added Vets Index Page. Closes #7

---
+++ pet-clinic-web/src/main/java/adi/art/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package adi.art.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+
+    @GetMapping("/vets")
+    public String listVets(){
+        return "vets/index";
+    }
+}
